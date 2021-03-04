47c31cdb7f824d2086113e731bd247fac4e2a6b3
jhpung <jhpung.dev@gmail.com>
2021-03-04 18:41:31 +0900

    Edit request limit size

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   app.enableCors();
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
